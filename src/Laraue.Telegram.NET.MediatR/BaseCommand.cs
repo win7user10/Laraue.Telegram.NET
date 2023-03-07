b30a788925a143d7bccfe382361ec3b5d7521f08
Ilya Belyanskiy <win7user20@gmail.com>
2023-03-07 23:31:32 +0300

    generic user id support for mediatr package

--- src/Laraue.Telegram.NET.MediatR/BaseCommand.cs
+++ src/Laraue.Telegram.NET.MediatR/BaseCommand.cs
@@ -5,13 +5,14 @@ namespace Laraue.Telegram.NET.MediatR;
 /// <summary>
 /// This class should be inherited by any telegram request handler.
 /// </summary>
+/// <typeparam name="TUserKey"></typeparam>
 /// <typeparam name="TData"></typeparam>
-public record BaseCommand<TData> : IRequest
+public record BaseCommand<TUserKey, TData> : IRequest where TUserKey : IEquatable<TUserKey>
 {
     /// <summary>
     /// User identifier in the database.
     /// </summary>
-    public string? UserId { get; init; }
+    public TUserKey? UserId { get; init; }
 
     /// <summary>
     /// Data required to execute this command.
