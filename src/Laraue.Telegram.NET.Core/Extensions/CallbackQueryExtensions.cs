50114df4aadab46294db5eefb5c3847351a6e4a5
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2023-12-12 16:42:52 +0300

    CallbackQuery extension GetMessageId() added

---
+++ src/Laraue.Telegram.NET.Core/Extensions/CallbackQueryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot.Types;
+
+namespace Laraue.Telegram.NET.Core.Extensions;
+
+/// <summary>
+/// Extensions for <see cref="CallbackQuery"/>.
+/// </summary>
+public static class CallbackQueryExtensions
+{
+    /// <summary>
+    /// Returns message identifier from the message related to the callback query.
+    /// </summary>
+    /// <param name="callbackQuery"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static int GetMessageId(this CallbackQuery? callbackQuery)
+    {
+        return callbackQuery?.Message?.MessageId
+            ?? throw new InvalidOperationException("Callback query is not passed");
+    }
+}
