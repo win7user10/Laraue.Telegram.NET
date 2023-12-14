af587e2001860bd376c2961a0c050a72679896b8
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2023-12-14 11:54:13 +0300

    migrate to net8

--- src/Laraue.Telegram.NET.Core/Routing/TelegramController.cs
+++ src/Laraue.Telegram.NET.Core/Routing/TelegramController.cs
@@ -1,11 +1,8 @@
-﻿using JetBrains.Annotations;
-
-namespace Laraue.Telegram.NET.Core.Routing;
+﻿namespace Laraue.Telegram.NET.Core.Routing;
 
 /// <summary>
 /// All possible controllers should be derived from that class.
 /// </summary>
-[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
 public abstract class TelegramController
 {
 }
