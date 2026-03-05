b7ebcd85f044bc214decedb768a0af2b3951ca18
Ilya Belyansky <win7user10@yandex.ru>
2026-03-05 19:53:55 +0300

    try fix pipes

--- src/Laraue.Telegram.NET.Core/Services/IUpdatesQueue.cs
+++ src/Laraue.Telegram.NET.Core/Services/IUpdatesQueue.cs
@@ -4,14 +4,23 @@ namespace Laraue.Telegram.NET.Core.Services;
 
 public interface IUpdatesQueue
 {
+    /// <summary>
+    /// Store updates to the queue.
+    /// </summary>
     Task AddAsync(
         IEnumerable<Update> updates,
         CancellationToken cancellationToken = default);
     
+    /// <summary>
+    /// Set the update state as successfully processed.
+    /// </summary>
     Task SetProcessedAsync(
         Update update,
         CancellationToken cancellationToken = default);
     
+    /// <summary>
+    /// Set the update state as failed.
+    /// </summary>
     Task SetFailedAsync(
         Update update,
         string error,
