f804041454fef035471ec16a60a23bffc3b519f2
DESKTOP-KT6GSMQ\Ilya <win7user10@yandex.ru>
2026-08-08 01:01:40 +0300

    feat: add TelegramInlineQueryRoute

---
+++ src/Laraue.Telegram.NET.Core/Routing/Attributes/TelegramInlineQueryRouteAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Laraue.Telegram.NET.Core.Routing.Attributes;
+
+/// <summary>
+/// Route based on the <see cref="Update.InlineQuery"/> property of <see cref="Update"/>.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class TelegramInlineQueryRouteAttribute : TelegramBaseRouteWithPathAttribute
+{
+    /// <summary>
+    /// This route will be matched when the message text is matching to the passed pattern.
+    /// </summary>
+    public TelegramInlineQueryRouteAttribute(string pathPattern)
+        : base(UpdateType.InlineQuery, RouteMethod.Get, pathPattern)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override string? GetDataStringFromUpdate(Update update)
+    {
+        return update.InlineQuery?.Query;
+    }
+}
